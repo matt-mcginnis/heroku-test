db8279aefba581ca7fd6452581807a30690bc62b
Matt McGinnis <mcginnisma90@gmail.com>
2018-10-23 15:49:29 -0400

    Updated routes and tested avatar upload.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'show_player' => 'players#show'
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'players#new'
+  root 'players#index'
 end
